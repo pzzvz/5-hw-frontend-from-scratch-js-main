4d4a5a52e89cdc8f8f514ae452fc7802023cbe1a
Artsiom Putsiata <48435837+pzzvz@users.noreply.github.com>
2024-11-26 23:04:35 +0300

    Update 02.js

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -13,7 +13,7 @@ let dice1 = Math.floor(Math.random() * 6) + 1;
 let dice2 = Math.floor(Math.random() * 6) + 1;
 
 // your code
-const isWinningDouble = (dice1 === dice2) && (dice1 >= 3) && (dice2 >= 3);
+const isWinningDouble = (dice1 === dice2) && (dice1 > 3);
 
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
