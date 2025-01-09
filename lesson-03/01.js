8aef5c2e29b0fc62de4035a806fee38b04e6c1e8
Artsiom Putsiata <48435837+pzzvz@users.noreply.github.com>
2025-01-09 16:42:03 +0300

    Update 01.js

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,6 +1,7 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage(numbers) {
-    const sum = numbers.reduce((acc, num) => acc + num, 0);
-    return sum / numbers.length;
+function calculateAverage() {
+    let sum = 0;
+            for ( i = 0; i < arguments[i]; i++ ) sum += arguments[i];
+            return sum == 0 ? sum : sum / arguments.length;
 }
